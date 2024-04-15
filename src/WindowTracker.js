fce5ccef4f7dc1335e5fe10ed0ade38c4954f16b
Katiasun <katjatolok3@gmail.com>
2024-04-15 13:01:35 +0200

    write code inside file WindowTracker.js

--- src/WindowTracker.js
+++ src/WindowTracker.js
@@ -1,5 +1,12 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export default function WindowTracker() {
-  return <h1>Window width: </h1>;
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    window.addEventListener("resize", function () {
+      setWindowWidth(window.innerWidth);
+    });
+  }, []);
+  return <h1>Window width: {windowWidth}</h1>;
 }
